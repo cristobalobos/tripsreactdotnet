d61408e207b24ec172e42a707fa899ede120377d
cristobalobos <cristobal.lobost@gmail.com>
2025-04-12 16:58:32 -0400

    feat: add initial Trips component with routing and basic render logic

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
+import { Trips } from "./components/Trips/Trips";
 
 import "./custom.css";
 
@@ -13,9 +14,10 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Trips} />
         <Route path="/counter" component={Counter} />
         <Route path="/fetch-data" component={FetchData} />
+        <Route path="/trips" component={Trips} />
       </Layout>
     );
   }
