8e06bf738e94abe12687d35dbf265361646b2a33
cristobalobos <cristobal.lobost@gmail.com>
2025-04-12 17:21:44 -0400

    read data from react

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path="/" component={Trips} />
+        <Route exact path="/" component={Home} />
         <Route path="/counter" component={Counter} />
         <Route path="/fetch-data" component={FetchData} />
         <Route path="/trips" component={Trips} />
