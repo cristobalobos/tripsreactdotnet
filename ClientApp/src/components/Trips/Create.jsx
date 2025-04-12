4455eb1ff2b45f38ca362f7671189e6324282a39
cristobalobos <cristobal.lobost@gmail.com>
2025-04-12 17:54:03 -0400

    feat: add trip creation form with input bindings and submission to API

--- ClientApp/src/components/Trips/Create.jsx
+++ ClientApp/src/components/Trips/Create.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Create extends Component {
     constructor(props) {
         super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDateStarted = this.onChangeDateStarted.bind(this);
+        this.onChangeDateCompleted = this.onChangeDateCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
             name: '',
@@ -10,6 +17,48 @@ export class Create extends Component {
             dateStarted: null,
             dateCompleted: null
         }
+    }
+
+    onChangeName(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDateStarted(e) {
+        this.setState({
+            dateStarted: e.target.value
+        });
+    }
+
+    onChangeDateCompleted(e) {
+        this.setState({
+            dateCompleted: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const { history } = this.props;
+
+        let tripObject = {
+            Id: Math.floor(Math.random() * 1000),
+            name: this.state.name,
+            description: this.state.description,
+            dateStarted: this.state.dateStarted,
+            dateCompleted: this.state.dateCompleted
+        }
+
+        axios.post("api/Trips/AddTrip", tripObject).then(result => {
+            history.push('/trips');
+        })
+
     }
 
     render() {
@@ -22,6 +71,8 @@ export class Create extends Component {
                         <input
                             type="text"
                             className="form-control"
+                            value={this.state.name}
+                            onChange={this.onChangeName}
                         />
                     </div>
                     <div className="form-group">
@@ -29,6 +80,8 @@ export class Create extends Component {
                         <textarea
                             type="text"
                             className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
                         />
                     </div>
                     <div className="row">
@@ -38,6 +91,8 @@ export class Create extends Component {
                                 <input
                                     type="date"
                                     className="form-control"
+                                    value={this.state.dateStarted}
+                                    onChange={this.onChangeDateStarted}
                                 />
                             </div>
                         </div>
@@ -47,6 +102,8 @@ export class Create extends Component {
                                 <input
                                     type="date"
                                     className="form-control"
+                                    value={this.state.dateCompleted}
+                                    onChange={this.onChangeDateCompleted}
                                 />
                             </div>
                         </div>
